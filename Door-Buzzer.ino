7445818ed60b1b7fc4261af657abe7c091d88b39
Siim Raud <2ndalpha@gmail.com>
2016-09-08 23:18:06 +0300

    Initial version

---
+++ Door-Buzzer.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASS "password"
+
+#define MQTT_CLIENT "Door-Buzzer"
+#define MQTT_SERVER "192.168.1.72"
+#define MQTT_PORT   1883
+#define MQTT_TOPIC  "home/doorbuzzer"
+
+#define RELAY 4
+#define BUZZER_ON_TIME 20 // In seconds
+#define CONNECTION_CHECK_INTERVAL 30 // In seconds
+
+extern "C" { 
+  #include "user_interface.h" 
+}
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient, MQTT_SERVER, MQTT_PORT);
+unsigned long timeBuzzerTurnedOn = 0;
+unsigned long lastConnectionCheckTime = 0;
+
+bool connectToWifi();
+void connectToMqtt();
+void mqttCallback(const MQTT::Publish&);
+void turnOffBuzzer();
+void checkConnection();
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(RELAY, OUTPUT);
+  
+  mqttClient.set_callback(mqttCallback);
+  if (connectToWifi()) {
+    connectToMqtt();
+  }
+}
+
+void loop() {
+  mqttClient.loop();
+
+  if (timeBuzzerTurnedOn > 0 && millis() > timeBuzzerTurnedOn + 1000 * BUZZER_ON_TIME) {
+    turnOffBuzzer();
+  }
+
+  if (millis () > lastConnectionCheckTime + 1000 * CONNECTION_CHECK_INTERVAL) {
+    checkConnection();
+  }
+}
+
+bool connectToWifi() {
+  Serial.println("Connecting to Wifi");
+  int retries = 10;
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while ((WiFi.status() != WL_CONNECTED) && retries--) {
+    delay(500);
+    Serial.print(" .");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) { 
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    return true;
+  }
+
+  Serial.println("Failed to connect to Wifi");
+  return false;
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT");
+  int retries = 10;
+
+  while (!mqttClient.connect(MQTT::Connect(MQTT_CLIENT).set_keepalive(90)) && retries--) {
+    Serial.print(" .");
+    delay(1000);
+  }
+
+  if(mqttClient.connected()) {
+    Serial.println("Connected to MQTT");
+    mqttClient.subscribe(MQTT_TOPIC);
+  }
+}
+
+void mqttCallback(const MQTT::Publish& pub) {
+  Serial.print("MQTT message: ");
+  Serial.println(pub.payload_string());
+
+  if (pub.payload_string() == "on") {
+    Serial.println("Turning buzzer on");
+    digitalWrite(RELAY, HIGH);
+    timeBuzzerTurnedOn = millis();
+  }
+}
+
+void turnOffBuzzer() {
+  Serial.println("Turning buzzer off");
+  digitalWrite(RELAY, LOW);
+  timeBuzzerTurnedOn = 0;
+}
+
+void checkConnection() {
+  if (WiFi.status() != WL_CONNECTED)  {
+    connectToWifi();
+  }
+  if (!mqttClient.connected()) {
+    connectToMqtt();
+  }
+
+  lastConnectionCheckTime = millis();
+}
+
---
+++ build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+
+ARDUINO_IDE_VERSION=1.6.9
+BUILD_DIR="build"
+
+if [ $(uname) == 'Darwin' ]
+then
+    PLATFORM='macosx'
+    FILE_EXT='zip'
+    ARDUINO_DIR='Arduino.app/Contents/Java'
+else
+    PLATFORM='linux64'
+    FILE_EXT='tar.xz'
+    ARDUINO_DIR="arduino-$ARDUINO_IDE_VERSION"
+fi
+
+if [ ! -e "$BUILD_DIR" ]
+then
+    mkdir $BUILD_DIR
+fi
+cd $BUILD_DIR
+
+if [ ! -e "$ARDUINO_DIR" ]
+then
+    ARDUINO_FILE=arduino-${ARDUINO_IDE_VERSION}-$PLATFORM.$FILE_EXT
+    wget -O $ARDUINO_FILE http://arduino.cc/download.php?f=/$ARDUINO_FILE
+
+    if [ $FILE_EXT == 'zip' ]
+    then
+        unzip $ARDUINO_FILE
+    else
+        tar xf $ARDUINO_FILE
+    fi
+
+    rm -f $ARDUINO_FILE
+fi
+
+if [ ! -e "esp8266" ]
+then
+    git clone https://github.com/esp8266/Arduino.git
+    mv Arduino esp8266
+    cd esp8266/tools
+    python get.py
+    cd ../../
+fi
+
+if [ ! -e "makeEspArduino" ]
+then
+    git clone https://github.com/plerup/makeEspArduino.git
+fi
+
+export ESP_ROOT=$(pwd)/esp8266
+#export VERBOSE=1
+export SINGLE_THREAD=1
+
+if [ ! -e "pubsubclient" ]
+then
+git clone https://github.com/Imroy/pubsubclient
+fi
+
+cd makeEspArduino
+
+export SKETCH=Door-Buzzer.ino
+export UPLOAD_PORT=/dev/cu.usbserial-A92HD3JZ
+export LIBS="$ESP_ROOT/libraries/ESP8266WiFi/ ../pubsubclient/src/"
+
+cp ../../*.ino .
+make -f makeEspArduino.mk upload
+
+screen $UPLOAD_PORT 115200
